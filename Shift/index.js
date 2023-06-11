0a3408e9fa95787ba25b4ff51a625ec7c4dc1958
Yutassk <sasaki.yt01@gmail.com>
2023-06-11 17:37:06 +0900

    calendar creat,btn adjust

--- Shift/index.js
+++ Shift/index.js
@@ -1,4 +1,5 @@
 'use strict';
+// Calendar
 const weeks = ['日', '月', '火', '水', '木', '金', '土']
 const date = new Date();
 const year = date.getFullYear();
@@ -7,26 +8,31 @@ const startDate = new Date(year, month - 1, 1); //月初めの日付
 const endDate = new Date(year, month, 0); //月末の日付
 const endDayCount = endDate.getDate(); //月の末日
 const startDay = startDate.getDay(); //月初めの曜日
+const today = date.getDate(); //当日の日付
 let dayCount = 1; //日付のカウント
 let calendarHtml = '';
 
 calendarHtml += `<h1>${year}年${month}月</h1>`;
-calendarHtml += '<table>';
+calendarHtml += '<table class="calendar">';
+calendarHtml += '<>';
+
 
 // 曜日の行
 for (let i = 0; i < weeks.length; i++) {
-    calendarHtml += `<td>${weeks[i]}</td>`;
+    calendarHtml += `<th class='week'>${weeks[i]}</th>`;
 };
 
-// 5週間分のtr
 for (let w = 0; w < 6; w++) {
     calendarHtml += '<tr>'
 
     for (let d = 0; d < 7; d++) {
-        if (w === 0 && d < startDay) {   //月初より前
+        if (w === 0 && d < startDay) {   //月初より前に空白
             calendarHtml += '<td>' + '</td>';
-        } else if (dayCount > endDayCount) {  // 月末日以降
+        } else if (dayCount > endDayCount) {  // 月末日以降に空白
             calendarHtml += '<td>' + '</td>';
+        } else if(dayCount < today) {  //当日より前をグレーアウトさせるためのクラス
+            calendarHtml += `<td class='gray'>${dayCount}</td>`;
+            dayCount++;
         } else {
             calendarHtml += `<td>${dayCount}</td>`;
             dayCount++;

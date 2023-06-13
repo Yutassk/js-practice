a9c195520f47e21a60b7f858f1f38cd691714936
Yutassk <sasaki.yt01@gmail.com>
2023-06-13 17:19:39 +0900

    todayオンオフ実装

--- calendar/index.js
+++ calendar/index.js
@@ -4,7 +4,6 @@ const $doc = document;
 const $nextBtn = $doc.querySelector('#nextBtn');
 const $prevBtn = $doc.querySelector('#prevBtn');
 
-
 // Calendar
 const weeks = ['日', '月', '火', '水', '木', '金', '土']
 const $week = $doc.querySelectorAll('.week');
@@ -16,35 +15,82 @@ let endDate = new Date(year, month, 0); //月末の日付
 let endDayCount = endDate.getDate(); //月の末日
 let startDay = startDate.getDay(); //月初めの曜日
 const today = date.getDate(); //当日の日付
-let $yearAndMonth = $doc.querySelector('#yearAndMonth');
+let $yearAndMonth = $doc.querySelector('#yearAndMonth'); //年月のh1書き換え用
 
-let dayCount = 1; //日付のカウント
+const thisYear = date.getFullYear();
+const thisMonth = date.getMonth() + 1;
 
-$yearAndMonth.textContent = `${year}年${month}月`;
-
-// 曜日を入力
+// 曜日表示
 for (let i = 0; i < weeks.length; i++) {
     $week[i].textContent = weeks[i];
-};
+}
 
-// td取りたい
-const $dateText = $doc.querySelectorAll('#dateText');
+const $dateText = $doc.querySelectorAll('#dateText'); //日付入力のtd
 
-for (let d = 0; d < $dateText.length; d++) {
-    if (d < startDay) {
-        $dateText[d].textContent = '';
-    } else if (dayCount > endDayCount) {
-        $dateText[d].textContent = ''
-    } else {
-        $dateText[d].textContent = dayCount;
-        dayCount++;
-    }
-    if(dayCount === today) {
-        console.log($dateText[d].textContent)
-        $dateText[d+1].classList.add('today');
-    }
-    if($dateText[dayCount].textContent < today) {
-        $dateText[dayCount].classList.add('gray');
-    }
-};
+function calendar() {
+    let dayCount = 1; //日付のカウント
 
+    $yearAndMonth.textContent = `${year}年${month}月`;
+
+    for (let d = 0; d < $dateText.length; d++) {
+        if (d < startDay) {
+            $dateText[d].textContent = '';
+        } else if (dayCount > endDayCount) {
+            $dateText[d].textContent = ''
+        } else {
+            $dateText[d].textContent = dayCount;
+            dayCount++;
+        }
+    }
+
+    // today付与
+    if (thisYear == year && thisMonth == month) {
+        $dateText.forEach(function (value) {
+            if (value.textContent == today) {
+                $dateText[today + startDay - 1].classList.toggle('today');
+            }
+        })
+    }
+
+    // 当月以外のtoday削除
+    if (thisMonth != month) {
+        $dateText.forEach((value) => {
+            if (value.classList.contains('today') == true) {
+                $doc.querySelector('.today').classList.remove('today');
+            }
+        })
+    }
+}
+
+// 次月へ進む
+const next = () => {
+    $nextBtn.addEventListener('click', (e) => {
+        month = month + 1;
+        startDate = new Date(year, month - 1, 1); //月初めの日付
+        endDate = new Date(year, month, 0); //月末の日付
+        endDayCount = endDate.getDate(); //月の末日
+        startDay = startDate.getDay(); //月初めの曜日
+
+        e.preventDefault();
+
+        calendar();
+    })
+}
+
+// 前月へ戻る
+const prev = () => {
+    $prevBtn.addEventListener('click', (e) => {
+        month = month - 1;
+        startDate = new Date(year, month - 1, 1); //月初めの日付
+        endDate = new Date(year, month, 0); //月末の日付
+        endDayCount = endDate.getDate(); //月の末日
+        startDay = startDate.getDay(); //月初めの曜日
+
+        e.preventDefault();
+
+        calendar();
+    })
+}
+calendar();
+next();
+prev();
